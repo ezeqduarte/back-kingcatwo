0a536af9e44e2ed05b16a8829e53dc6cd77c5c42
matiasecharri <matiasecharri@hotmail.com>
2022-11-11 20:21:11 -0300

    se creo ruta*controlador para show

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ let router = require("express").Router()
 let user = require("./users")
 let hotel = require("./hotels")
 let city = require ("./cities")
+let show = require ("./shows")
 
 
 router.use("/hotels", hotel)
 router.use("/users", user)
 router.use("/cities", city)
+router.use("/shows", show)
 
 
 
