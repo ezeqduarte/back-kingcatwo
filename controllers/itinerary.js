11d1967138a329ba7b9c0d9bc6397960b879e687
Your Name <ezeqduarte01@gmail.com>
2022-11-12 13:47:31 -0300

    controlador listo para eliminar un itinerario

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -2,7 +2,6 @@ const Itinerary = require("../models/Itinerary");
 
 const controller = {
   create: async (req, res) => {
-   
     try {
       let new_itinerary = await Itinerary.create(req.body);
       res.status(201).json({
@@ -14,6 +13,26 @@ const controller = {
       res.status(400).json({
         success: false,
         message: error.message,
+      });
+    }
+  },
+
+  deleteItinerary: async (req, res) => {
+
+    const _id = req.params.id;    
+
+    try {
+      
+      await Itinerary.findOneAndDelete({_id});
+
+      res.status(200).json({
+        idDeleted: _id,        
+        success: true,
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        response: error.message,
       });
     }
   },
