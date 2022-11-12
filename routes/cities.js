850c69b786bb1a2adff785b5281ae0fda0f6d898
Your Name <ezeqduarte01@gmail.com>
2022-11-12 20:05:59 -0300

    todo terminado

--- routes/cities.js
+++ routes/cities.js
@@ -2,9 +2,11 @@ const controller = require("../controllers/city");
 
 let router = require("express").Router()
 
-let {create, updateCity}=controller
+let {create, updateCity , deleteCity}=controller
 
 router.route("/").post(create)
 router.route("/:id").put(updateCity)
+router.route("/:id").delete(deleteCity)
+
 
 module.exports = router;
