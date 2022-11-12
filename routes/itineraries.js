5a527ef9298993dbb8f0357a008b242c3676a6ba
Your Name <ezeqduarte01@gmail.com>
2022-11-12 16:26:25 -0300

    ruta creada para updatear un itinerario

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -2,9 +2,11 @@ const controller = require("../controllers/itinerary");
 
 let router = require("express").Router()
 
-let {create, deleteItinerary}=controller
+let {create, deleteItinerary , updateItinerary}=controller
 
 router.route("/").post(create)
 router.route("/:id").delete(deleteItinerary)
+router.route("/:id").put(updateItinerary)
+
 
 module.exports = router;
