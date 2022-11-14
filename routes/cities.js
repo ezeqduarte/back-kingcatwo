dc3db5700c42786ad56bad6fd5c1da7ea1b45ffd
Your Name <ezeqduarte01@gmail.com>
2022-11-14 03:40:58 -0300

    controller and rutes para agarrar una ciudad

--- routes/cities.js
+++ routes/cities.js
@@ -2,11 +2,12 @@ const controller = require("../controllers/city");
 
 let router = require("express").Router()
 
-let {create, readCities , updateCity , deleteCity}=controller
+let {create, readCities , updateCity , deleteCity , readCity}=controller
 
 router.route("/").post(create)
 router.route("/").get(readCities)
 router.route("/:id").put(updateCity)
+router.route("/:id").get(readCity)
 router.route("/:id").delete(deleteCity)
 
 
