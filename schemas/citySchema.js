2c5b26cce0f95739ff4fe1688f28820f9f0f1397
Your Name <ezeqduarte01@gmail.com>
2022-11-20 21:02:38 -0300

    fix

--- schemas/citySchema.js
+++ schemas/citySchema.js
@@ -3,22 +3,22 @@ const joi = require("joi");
 const schema = joi.object({
   name: joi.string().pattern(/[a-z]/).required().min(3).max(45).messages({
     "any.required": "Name is required",
-    "string.base": "Esto es una prueba del ano que me duele",
+    "string.base": "Name is empty",
     "string.min": "Name is too short",
     "string.max": "Name is too long",
     "string.pattern.base": "Name is not a number",
   }),
   continent: joi.string().pattern(/[a-z]/).required().min(4).max(10).messages({
     "any.required": "Continent is required",
-    "string.base": "Esto es una prueba del ano que me duele",
+    "string.base": "Continent is empty",
     "string.min": "Continent is too short",
     "string.max": "Continent is too long",
     "string.pattern.base": "Continent is not a number",
   }),
   photo: joi.string().required().messages({
     "any.required": "Photo is required",
-    "string.base": "Esto es una prueba del ano que me duele",
-    "string.base": "Please insert a valid URL photo",
+    "string.base": "photo is empty",
+
   }),
   population: joi.number().required().messages({
     "any.required": "Population is required",
@@ -26,8 +26,8 @@ const schema = joi.object({
   }),
   userId: joi.string().required().messages({
     "any.required": "UserId is required",
-    "string.base": "Esto es una prueba del ano que me duele",
-    "string.base": "UserId must be a string",
+    "string.base": "UserId is empty",
+
   }),
 });
 
