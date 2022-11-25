980dae5f5afbcb409ffb94451f6cf8c5a8a09741
matiasecharri <matiasecharri@hotmail.com>
2022-11-25 16:28:30 -0300

    Implementado token

---
+++ middlewares/accountExistSignIn.js
@@ -0,0 +1,22 @@
+const User= require("../models/User");
+const { invalidCredentialsResponse } = require("../config/responses")
+
+async function accountExistsSignIn(req, res, next) {
+    const user = await User.findOne({email: req.body.email})
+    if (user) {
+        req.user = { //inyecto al req la propiedad user con los datos que necesito
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            photo: user.photo,
+            password: user.password,
+            verified: user.verified,
+            role: user.role,
+        }
+        //console.log(req.user)
+        return next()
+    }
+    return invalidCredentialsResponse(req,res)
+}
+
+module.exports = accountExistsSignIn
