77b55cd011cc27c97534d3eab2d627d2f7f6d7c3
Your Name <ezeqduarte01@gmail.com>
2022-11-24 22:47:14 -0300

    mt0145 terminada

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,11 @@
 const User = require("../models/User");
+const bcryptjs = require("bcryptjs");
+const crypto = require("crypto");
+const accountVerificationEmail = require("./accountVerificationEmail");
+const { userSignedUpResponse } = require("../config/responses");
 
 const controller = {
-  create: async (req, res) => {
+  /*  create: async (req, res) => {
     try {
       let new_user = await User.create(req.body);
       res.status(201).json({
@@ -14,6 +18,33 @@ const controller = {
         success: false,
         message: error.message,
       });
+    }
+  }, */
+
+  registerUser: async (req, res, next) => {
+    let { name, lastName, role, photo, age, email, password } = req.body;
+    let verified = false;
+    let logged = false;
+    let code = crypto.randomBytes(10).toString("hex");
+    password = bcryptjs.hashSync(password, 10);
+
+    try {
+      await User.create({
+        name,
+        lastName,
+        photo,
+        age,
+        role,
+        email,
+        password,
+        verified,
+        logged,
+        code,
+      });
+      await accountVerificationEmail(email, code);
+      return userSignedUpResponse(req, res);
+    } catch (error) {
+      next(error);
     }
   },
 };
