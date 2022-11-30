63ded235c7375084c7597d4fcaf422d526560a6c
Your Name <ezeqduarte01@gmail.com>
2022-11-30 12:36:04 -0300

    routas y controladores para reactiones especificas

--- controllers/reaction.js
+++ controllers/reaction.js
@@ -3,14 +3,88 @@ const Reaction = require("../models/Reaction");
 const controller = {
   newReaction: async (req, res) => {
     try {
-      
-        let newReaction = await Reaction.create(req.body);
-        res.status(201).json({
-            reaction: newReaction,
-            success: true,
-            message: "The reaction was succefuly created",
-          });
+      let newReaction = await Reaction.create(req.body);
+      res.status(201).json({
+        reaction: newReaction,
+        success: true,
+        message: "The reaction was succefuly created",
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+        error: error.status,
+      });
+    }
+  },
 
+  postReaction: async (req, res) => {
+    let query = {};
+
+    if (req.query.name) {
+      query = { ...query, name: req.query.name };
+    }
+
+    if (req.query.itineraryId) {
+      query = { ...query, itineraryId: req.query.itineraryId };
+    }
+
+    try {
+      let reactionOfTinerary = await Reaction.findOne(query);
+      if (reactionOfTinerary) {
+        if (reactionOfTinerary.userId.includes(req.user.id)) {
+          //tendria que sacarlo
+          await Reaction.findOneAndUpdate(
+            { _id: reactionOfTinerary._id },
+            { $pull: { userId: req.user.id } },
+            { new: true }
+          );
+          res.status(200).json({
+            reaction: reactionOfTinerary,
+            success: true,
+            message: "The reaction modificated dislike",
+          });
+        } else {
+          //tendria que pushearlo
+          await Reaction.findOneAndUpdate(
+            { _id: reactionOfTinerary._id },
+            { $push: { userId: req.user.id } },
+            { new: true }
+          );
+          res.status(200).json({
+            reaction: reactionOfTinerary,
+            success: true,
+            message: "The reaction modificated like",
+          });
+        }
+      } else {
+        res.status(404).json({         
+          success: false,
+          message: "The reaction dosnt exist",
+        });
+      }
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+
+  getReactions: async (req, res) => {
+    let query = {}
+
+    if (req.query.itineraryId) {
+      query = { ...query, itineraryId: req.query.itineraryId };
+    }
+
+    try {
+      let reactions = await Reaction.find(query);
+      res.status(201).json({
+        reactions: reactions,
+        success: true,
+        message: "The reaction are here",
+      });
     } catch (error) {
       res.status(400).json({
         success: false,
--- routes/reactions.js
+++ routes/reactions.js
@@ -2,12 +2,14 @@ let router = require("express").Router();
 const schema = require("../schemas/reactionSchema");
 const validator = require("../middlewares/validator");
 const controller = require("../controllers/reaction");
+const passport = require("../config/passport");
 
-let { newReaction } = controller;
+let { newReaction, postReaction, getReactions } = controller;
 
-router.post("/", validator(schema) , newReaction);
-
-
-
+router.post("/", validator(schema), newReaction);
+router.put("/", passport.authenticate("jwt", { session: false }), postReaction);
+router.get("/", getReactions);
 
 module.exports = router;
+
+
