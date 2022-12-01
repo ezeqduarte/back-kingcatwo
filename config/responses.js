cdb6dafe28748af7c4fe3e0c416b91a6dfb0b4f1
Your Name <ezeqduarte01@gmail.com>
2022-12-01 15:35:35 -0300

    avances mt-01-56

--- config/responses.js
+++ config/responses.js
@@ -1,60 +1,74 @@
-function userExistsResponse(req,res) {
-    return res.status(200).json({
-        success: false,
-        message: ['User already exists']
-    })
+function userExistsResponse(req, res) {
+  return res.status(200).json({
+    success: false,
+    message: ["User already exists"],
+  });
 }
 
-function userSignedUpResponse(req,res) {
-    return res.status(201).json({
-        success: true,
-        message: 'user signed up'
-    })
+function userSignedUpResponse(req, res) {
+  return res.status(201).json({
+    success: true,
+    message: "user signed up",
+  });
 }
 
-function userSignedOutResponse(req,res) {
-    return res.status(201).json({
-        success: true,
-        message: 'user signed out'
-    })
+function userSignedOutResponse(req, res) {
+  return res.status(201).json({
+    success: true,
+    message: "user signed out",
+  });
 }
 
-function userNotFoundResponse(req,res) {
-    return res.status(404).json({
-        success: false,
-        message: 'user not found'
-    })
+function userNotFoundResponse(req, res) {
+  return res.status(404).json({
+    success: false,
+    message: "user not found",
+  });
 }
 
-function mustSignInResponse(req,res) {
-    return res.status(400).json({
-        success: false,
-        message: 'sign in please!'
-    })
+function mustSignInResponse(req, res) {
+  return res.status(400).json({
+    success: false,
+    message: "sign in please!",
+  });
 }
 
-function invalidCredentialsResponse(req,res) {
-    return res.status(200).json({
-        success: false,
-        message: ['User or password not found!'],
-    })
+function invalidCredentialsResponse(req, res) {
+  return res.status(200).json({
+    success: false,
+    message: ["User or password not found!"],
+  });
 }
 
-function verifyResponse(req,res) {
-    return res.status(401).json({
-        success: false,
-        message: 'Please, verify your email account and try again'
-    })
+function verifyResponse(req, res) {
+  return res.status(401).json({
+    success: false,
+    message: "Please, verify your email account and try again",
+  });
+}
+
+function operationUnauthorized(req, res) {
+  return res.status(401).json({
+    success: false,
+    message: "Cant realice this operation",
+  });
+}
+
+function operationNotFound(req, res) {
+  return res.status(404).json({
+    success: false,
+    message: "Couldn't find the operationNotFound",
+  });
 }
 
 module.exports = {
-    userSignedUpResponse,
-    userExistsResponse,
-    userNotFoundResponse,
-    userSignedOutResponse,
-    mustSignInResponse,
-    invalidCredentialsResponse,
-    verifyResponse
-}
-
-
+  userSignedUpResponse,
+  userExistsResponse,
+  userNotFoundResponse,
+  userSignedOutResponse,
+  mustSignInResponse,
+  invalidCredentialsResponse,
+  verifyResponse,
+  operationUnauthorized,
+  operationNotFound,
+};
